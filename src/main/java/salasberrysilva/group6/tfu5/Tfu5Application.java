f76a3bfb8d48af558024de8c1dd25fcdc61efb8f
Nicolás Wagner <nicolas.wagner@correo.ucu.edu.uy>
2026-06-17 15:34:20 -0300

    spring-boot project initialized

---
+++ src/main/java/salasberrysilva/group6/tfu5/Tfu5Application.java
@@ -0,0 +1,13 @@
+package salasberrysilva.group6.tfu5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tfu5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tfu5Application.class, args);
+	}
+
+}
---
+++ src/test/java/salasberrysilva/group6/tfu5/Tfu5ApplicationTests.java
@@ -0,0 +1,13 @@
+package salasberrysilva.group6.tfu5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tfu5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
